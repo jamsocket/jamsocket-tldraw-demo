01be88c430a6a44b18c9ec388a5b27f9da4a73f2
Paul Butler <paul@driftingin.space>
2024-12-13 15:04:26 -0500

    Add prettier (#3)

--- server/src/rooms.ts
+++ server/src/rooms.ts
@@ -2,47 +2,47 @@ import { RoomSnapshot, TLSocketRoom } from '@tldraw/sync-core'
 import { readFromS3, writeToS3 } from './persist'
 
 interface RoomState {
-	room: TLSocketRoom
-	needsPersist: boolean
+  room: TLSocketRoom
+  needsPersist: boolean
 }
 
 export async function getRoomState(): Promise<RoomState> {
-	let initialSnapshotJson = await readFromS3() ?? undefined
+  let initialSnapshotJson = (await readFromS3()) ?? undefined
 
-	let initialSnapshot: RoomSnapshot | undefined
-	if (typeof initialSnapshotJson === 'string') {
-		initialSnapshot = JSON.parse(initialSnapshotJson) as RoomSnapshot
-	}
+  let initialSnapshot: RoomSnapshot | undefined
+  if (typeof initialSnapshotJson === 'string') {
+    initialSnapshot = JSON.parse(initialSnapshotJson) as RoomSnapshot
+  }
 
-	let roomState = {
-		room: new TLSocketRoom({
-			initialSnapshot,
-			onSessionRemoved(room, args) {
-				console.log('client disconnected', args.sessionId, 'default')
-				if (args.numSessionsRemaining === 0) {
-					console.log('closing room', 'default')
-					room.close()
-				}
-			},
-			onDataChange() {
-				roomState.needsPersist = true
-			},
-		}),
-		needsPersist: false,
-	}
+  let roomState = {
+    room: new TLSocketRoom({
+      initialSnapshot,
+      onSessionRemoved(room, args) {
+        console.log('client disconnected', args.sessionId, 'default')
+        if (args.numSessionsRemaining === 0) {
+          console.log('closing room', 'default')
+          room.close()
+        }
+      },
+      onDataChange() {
+        roomState.needsPersist = true
+      },
+    }),
+    needsPersist: false,
+  }
 
-	async function saveSnapshot(snapshot: RoomSnapshot) {
-		writeToS3(JSON.stringify(snapshot))
-	}
+  async function saveSnapshot(snapshot: RoomSnapshot) {
+    writeToS3(JSON.stringify(snapshot))
+  }
 
-	setInterval(() => {
-		if (roomState.needsPersist) {
-			// persist room
-			roomState.needsPersist = false
-			console.log('saving snapshot')
-			saveSnapshot(roomState.room.getCurrentSnapshot())
-		}
-	}, 10_000)
+  setInterval(() => {
+    if (roomState.needsPersist) {
+      // persist room
+      roomState.needsPersist = false
+      console.log('saving snapshot')
+      saveSnapshot(roomState.room.getCurrentSnapshot())
+    }
+  }, 10_000)
 
-	return roomState
+  return roomState
 }
--- server/src/unfurl.ts
+++ server/src/unfurl.ts
@@ -1,14 +1,14 @@
 import _unfurl from 'unfurl.js'
 
 export async function unfurl(url: string) {
-	const { title, description, open_graph, twitter_card, favicon } = await _unfurl.unfurl(url)
+  const { title, description, open_graph, twitter_card, favicon } = await _unfurl.unfurl(url)
 
-	const image = open_graph?.images?.[0]?.url || twitter_card?.images?.[0]?.url
+  const image = open_graph?.images?.[0]?.url || twitter_card?.images?.[0]?.url
 
-	return {
-		title,
-		description,
-		image,
-		favicon,
-	}
+  return {
+    title,
+    description,
+    image,
+    favicon,
+  }
 }
